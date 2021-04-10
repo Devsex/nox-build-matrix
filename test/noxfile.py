d44224c1cb6cfc50166a664754d909ca0048d691
Riley Flynn <riley@rileyflynn.me>
2021-04-10 13:54:43 -0230

    Add more test cases

--- test/noxfile.py
+++ test/noxfile.py
@@ -1,18 +1,33 @@
 import nox
 
+
 @nox.session(python=["3.6", "3.7", "3.8", "3.9"])
 def multi_version(session):
     pass
 
+
 @nox.session(python="3.9")
 def single_version(session):
     pass
+
 
 @nox.session(python="3.9")
 @nox.parametrize("param", ["a", "b", "c"])
 def parametrized(session, param):
     pass
 
+
 @nox.session(python="3.9")
-def ignored(session, param):
+@nox.parametrize("param", ["1", "2", "3"], ids=["one", "two", "three"])
+def parametrized_display_name(session, param):
+    pass
+
+
+@nox.session(python="3.9")
+def ignored(session):
+    pass
+
+
+@nox.session(python="3.9", name="custom_name")
+def renamed(session):
     pass
